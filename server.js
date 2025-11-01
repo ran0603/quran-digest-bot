0f14a0923e778aa4892e0bebfdefe8fd83ca4696
Razan <ran0603@tuta.io>
2025-11-01 17:40:44 +0800

    fix: Update server listen method to default to localhost

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Static server running on port ${PORT}`)
 })
